a40a30976cd377272a89365289f517dfed528ce6
MisterKermit <97013614+MisterKermit@users.noreply.github.com>
2025-06-16 21:27:09 -1000

    Wrote Hello world program

---
+++ helloworld.py
@@ -0,0 +1,3 @@
+print("Hello world!")
+
+print("So far, I am working on a project working with a local hospital's rehab center to create motorized wheelchair trainers to aid young children affected by motor disabiliites.")
